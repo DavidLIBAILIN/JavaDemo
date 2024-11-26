41268f3a8bed89555bb2adcf495a5abf31cba63a
Bolin Li <bolinli@MacBook-Air.local>
2024-11-25 19:18:59 -0500

    Remove Unused File

--- src/ExcepTest.java
+++
@@ -1,8 +0,0 @@
-public class ExcepTest {
-    public static void main(String[] args) {
-
-            int a[] = new int[2];
-            System.out.println("Access element three : " + a[3]);
-
-    }
-}
