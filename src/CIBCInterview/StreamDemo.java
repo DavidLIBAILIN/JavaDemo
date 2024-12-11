ffb6dfca7e8a96caee2efdfc5a6222316d150e2a
Bolin Li <davidlibailin@gmail.com>
2024-12-11 17:32:35 -0500

    Update

--- src/CIBCInterview/StreamDemo.java
+++ src/CIBCInterview/StreamDemo.java
@@ -1,8 +1,11 @@
 package CIBCInterview;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
     public static void main(String[] args) {
@@ -17,6 +20,16 @@ public class StreamDemo {
         memberNames.add("Lokesh");
         Predicate<String> p = (s) -> s.startsWith("A");
 
-        memberNames.stream().filter(p).forEach((s) -> System.out.println(s));
+        memberNames.stream().filter(p).map(String::toLowerCase).toList();
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 1; i< 10; i++){
+            list.add(i);
+        }
+
+//Here creating a parallel stream
+        Stream<Integer> stream = list.parallelStream();
+
+        Integer[] evenNumbersArr = stream.filter(i -> i%2 == 0).toArray(Integer[]::new);
+        System.out.print(Arrays.toString(evenNumbersArr));
     }
 }
